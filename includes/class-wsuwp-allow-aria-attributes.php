bbcfe3b40c2c63666ebb3c8915a109078405add2
Jeremy Felt <jeremy.felt@gmail.com>
2017-03-08 10:04:30 -0800

    Allow select aria attributes to be saved in the post context
    Filters `$allowedposttags` to include aria-describedby, aria-hidden,
    aria-label, and aria-labelledby.

--- includes/class-wsuwp-allow-aria-attributes.php
+++ includes/class-wsuwp-allow-aria-attributes.php
@@ -27,5 +27,56 @@ class WSUWP_Allow_Aria_Attributes {
 	 *
 	 * @since 0.0.1
 	 */
-	public function setup_hooks() {}
+	public function setup_hooks() {
+		add_filter( 'wp_kses_allowed_html', array( $this, 'allowed_aria_attributes' ), 10, 2 );
+	}
+
+	/**
+	 * Filters the list of allowed elements and attributes when saving content.
+	 *
+	 * @since 0.0.1
+	 *
+	 * @param array  $allowedposttags
+	 * @param string $context
+	 *
+	 * @return array
+	 */
+	public function allowed_aria_attributes( $allowedposttags, $context ) {
+		if ( 'post' !== $context ) {
+			return $allowedposttags;
+		}
+
+		$allowedposttags = array_map( 'WSUWP_Allow_Aria_Attributes::merge_allowed_aria_attributes', $allowedposttags );
+
+		return $allowedposttags;
+	}
+
+	/**
+	 * Merges a preset list of aria attributes into an allowed HTML element's array
+	 * of allowed attributes.
+	 *
+	 * @since 0.0.1
+	 *
+	 * @param bool|array $value
+	 *
+	 * @return array
+	 */
+	public static function merge_allowed_aria_attributes( $value ) {
+		$aria_attributes = array(
+			'aria-label' => true,
+			'aria-labelledby' => true,
+			'aria-hidden' => true,
+			'aria-describedby' => true,
+		);
+
+		if ( true === $value ) {
+			$value = array();
+		}
+
+		if ( is_array( $value ) ) {
+			return array_merge( $value, $aria_attributes );
+		}
+
+		return $value;
+	}
 }
--- tests/test-wsuwp-allow-aria-attributes.php
+++ tests/test-wsuwp-allow-aria-attributes.php
@@ -1,10 +1,60 @@
 <?php
 
 class WSUWP_Allow_Aria_Attributes_Tests extends WP_UnitTestCase {
-	/**
-	 * An initial sample test to verify working tests.
-	 */
-	public function test_wsuwp_allow_aria_attributes() {
-		$this->assertTrue( true );
+
+	public function test_merge_allowed_aria_attributes_with_true_value_returns_aria_array() {
+		$value = WSUWP_Allow_Aria_Attributes::merge_allowed_aria_attributes( true );
+
+		$expected = array(
+			'aria-label' => true,
+			'aria-labelledby' => true,
+			'aria-hidden' => true,
+			'aria-describedby' => true,
+		);
+
+		$this->assertEquals( $value, $expected );
+	}
+
+	public function test_merge_allowed_aria_attributes_with_array_value_merges_arrays() {
+		$value = WSUWP_Allow_Aria_Attributes::merge_allowed_aria_attributes( array( 'class' => true, 'id' => true, 'data-test' => true ) );
+
+		$expected = array(
+			'class' => true,
+			'id' => true,
+			'data-test' => true,
+			'aria-label' => true,
+			'aria-labelledby' => true,
+			'aria-hidden' => true,
+			'aria-describedby' => true,
+		);
+
+		$this->assertEquals( $value, $expected );
+	}
+
+	public function test_merge_allowed_aria_attributes_with_string_value_does_nothing() {
+		$value = WSUWP_Allow_Aria_Attributes::merge_allowed_aria_attributes( 'div' );
+
+		$this->assertEquals( $value, 'div' );
+	}
+
+	public function test_wp_kses_post_allows_white_listed_aria_attribute() {
+		$expected = '<div aria-label="testing">Test Content</div>';
+		$actual = wp_kses_post( $expected );
+
+		$this->assertEquals( $expected, $actual );
+	}
+
+	public function test_wp_kses_post_removes_non_white_listed_aria_attribute() {
+		$expected = '<div aria-label="testing" aria-owns="an-element">Test Content</div>';
+		$actual = wp_kses_post( $expected );
+
+		$this->assertEquals( '<div aria-label="testing">Test Content</div>', $actual );
+	}
+
+	public function test_wp_kses_post_allows_other_white_listed_attributes() {
+		$expected = '<div aria-label="testing" class="more-testing">Test Content</div>';
+		$actual = wp_kses_post( $expected );
+
+		$this->assertEquals( $expected, $actual );
 	}
 }
